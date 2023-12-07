16f0c6fa186e3349af1360b7bd054c454a09587a
Sara <sara.krieger2022@gmail.com>
2023-12-07 15:08:58 +0100

    Moved files into different directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import './App.css'
-import {Blog} from "./assets/Blog.tsx";
-import {Profile} from "./assets/Profile.tsx";
+import {Blog} from "./Blog.tsx";
+import {Profile} from "./Profile.tsx";
 
 function App() {
 
